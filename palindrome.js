01218b1ab0c0e033574a8c708edf6ff942cd8970
Stephen P <stephenp@ZipcodersMBP79.fios-router.home>
2018-11-06 23:34:42 -0500

    complete

--- palindrome.js
+++ palindrome.js
@@ -1,6 +1,24 @@
 var isPalinDrome = function(theString){
 
-//code here
+    var palin = prompt("Eneter in your string");
+    palin = palin.toLocaleLowerCase();
+
+    var characterArr = palin.split("");
+    var validChars = "abcdefghijklmnopqrstuvwxyz".split("");
+
+    var lettersArr = [];
+
+    characterArr.forEach(function(charr){
+        if(validChars.indexOf(charr) > -1){
+            lettersArr.push(charr);
+        }
+    })
+
+    if(lettersArr.join("") === lettersArr.reverse().join("")){
+        document.getElementById("palindrometext").textContent = "TRUE";
+    }else{
+        document.getElementById("palindrometext").textContent = "FALSE";
+    }
 
 }
 
